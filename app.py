677807d3b7a2208b62c997bd166d06c93aff2c1a
keval patel <73472954+mrxpatel@users.noreply.github.com>
2025-08-11 16:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
-# Initialize tables (skipping 13 and 17)
+# Tables data (skipping 13 and 17)
 tables = {str(i): {'id': i, 'capacity': 4, 'billed': False, 'start': None}
           for i in list(range(1, 23)) if i not in (13, 17)}
-
 recentCleared = []
 
 @app.route('/')
@@ -27,7 +26,7 @@ def handle_start_table(data):
         tables[tid]['billed'] = True
         tables[tid]['start'] = int(time.time() * 1000)
         socketio.emit('tables_update', {
-            'tables': tables,
+            'tables': {tid: tables[tid]},
             'recentCleared': recentCleared
         }, broadcast=True)
 
@@ -42,9 +41,15 @@ def handle_clear_table(data):
         if len(recentCleared) > 10:
             recentCleared.pop()
         socketio.emit('tables_update', {
-            'tables': tables,
+            'tables': {tid: tables[tid]},
             'recentCleared': recentCleared
         }, broadcast=True)
+
+@socketio.on('remove_cleared_item')
+def handle_remove_cleared_item(item):
+    if item in recentCleared:
+        recentCleared.remove(item)
+        socketio.emit('recently_cleared_update', recentCleared, broadcast=True)
 
 @socketio.on('reset_all')
 def handle_reset_all():
@@ -56,15 +61,6 @@ def handle_reset_all():
         'tables': tables,
         'recentCleared': recentCleared
     }, broadcast=True)
-
-@socketio.on('remove_cleared_item')
-def handle_remove_cleared_item(item):
-    if item in recentCleared:
-        recentCleared.remove(item)
-        socketio.emit('tables_update', {
-            'tables': tables,
-            'recentCleared': recentCleared
-        }, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
